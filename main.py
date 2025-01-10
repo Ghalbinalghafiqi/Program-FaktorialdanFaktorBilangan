b8cfdbe1b74153e337a8dd1bda4f47609a270f91
Galbin <galbin@gmail.com>
2025-01-10 23:05:19 +0700

    menambahkan program faktor bilangan

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-def fa(n):
+def factorial(n):
     if n == 0:
         return 1
     else:
-        return n * fa(n - 1)
+        return n * factorial(n - 1)
 
-n = int(input("Masukkan Nilai = "))
+def faktor(n):
+    return [i for i in range(1, n + 1) if n % i == 0]
 
-print(n, "! =", fa(n))
+while True:
+    n = int(input("Masukkan Nilai = "))
+    print(f"{n}! = {factorial(n)}")
+    print(f"Faktor dari {n} adalah: {faktor(n)}")
+    print()  
